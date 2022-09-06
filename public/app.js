b49d10dfd673caba42e64982a03719825ce50639
isacchr <isac.christiansson@gmail.com>
2022-09-06 10:33:33 +0200

    refactored using code to generate HTMl

--- public/app.js
+++ public/app.js
@@ -1,5 +1,14 @@
 console.log('Hey from javascript');
 
 function sayHello() {
- console.log('sayHello called .. ');
+    const contactPersonName = contactPerson.value;
+   
+    document.getElementById('namePlaceholder').innerHTML = contactPersonName;
+
+    const target = document.getElementById('app');
+    const newElement = document.createElement('p');
+    newElement.innerHTML = 'TESTING';
+    newElement.className = 'red';
+    target.appendChild(newElement);
+
 }
